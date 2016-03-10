84beec0ac0a005fe99ccc43b57a207d63232ce48
manishishere <you@example.com>
2016-03-10 16:56:50 +0530

    First commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'url' : 'mongodb://127.0.0.1:27017/manish' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+   // mongoose.connect('mongodb://127.0.0.1:27017/manish');
+
+
+
+
+};
